b15f648451af7b26dcbb79cc244c974c46b4608f
Harry <harry@Harrys-MacBook-Pro.local>
2021-09-14 21:26:03 +0530

    checkoutSessionAdded

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
-const express = require('express');
-const cors = require('cors');
-const Stripe = require('stripe');
+const express = require("express");
+const cors = require("cors");
+const Stripe = require("stripe");
 const stripeSecretKey =
-  'sk_test_51JZTWQSIUZLasS08u9zNVrRNGjlNZ3u9CiijMHpdMAzeEMIPPvs6AKqaKoGm1yL5IQ8gRxW4CMHh35hNCjrsP7e300LYhor1Mi';
-const asyncHanlder = require('express-async-handler');
+  "sk_test_51JZTWQSIUZLasS08u9zNVrRNGjlNZ3u9CiijMHpdMAzeEMIPPvs6AKqaKoGm1yL5IQ8gRxW4CMHh35hNCjrsP7e300LYhor1Mi";
+const asyncHanlder = require("express-async-handler");
 
 const app = express();
 
 app.use(cors());
-
-// app.get('/', (req, res) => {
-//   res.status(200).json({
-//     status: 'Success',
-//     message: 'Welcome to stripe Test',
-//   });
-// });
 
 const createCharges = asyncHanlder(async (req, res, next) => {
   const { name } = req.query;
@@ -25,35 +18,35 @@ const createCharges = asyncHanlder(async (req, res, next) => {
   const customer = await stripe.customers.create({
     name: name,
     address: {
-      city: 'Chennai',
-      country: 'India',
-      line1: '123 , Abcd street',
-      postal_code: '123456',
-      state: 'TamilNadu',
+      city: "Chennai",
+      country: "India",
+      line1: "123 , Abcd street",
+      postal_code: "123456",
+      state: "TamilNadu",
     },
     // currency: 'inr',
     email: `${name}@test.com`,
-    description: 'My Test Customer',
+    description: "My Test Customer",
     metadata: {
-      productId: '123abcd456efgh',
+      productId: "123abcd456efgh",
     },
   });
   console.log(customer);
   res.status(200).json({
-    status: 'Success',
-    message: 'Good to Go',
+    status: "Success",
+    message: "Good to Go",
   });
 });
 
 const getCustomerFromStripe = asyncHanlder(async (req, res, next) => {
   const stripe = Stripe(stripeSecretKey);
 
-  const customer = await stripe.customers.retrieve('cus_KDyBaDM9BFq1C0');
+  const customer = await stripe.customers.retrieve("cus_KE5AtPD46hCuWa");
 
   console.log(customer);
 
   res.status(200).json({
-    status: 'Success',
+    status: "Success",
     data: customer,
   });
 });
@@ -62,21 +55,73 @@ const createSubscription = asyncHanlder(async (req, res, next) => {
   const stripe = Stripe(stripeSecretKey);
 
   const subscription = await stripe.subscriptions.create({
-    customer: 'cus_KDyBaDM9BFq1C0',
+    customer: "cus_KE20K5N0JJjH7S",
     // default_source: 'card_18NVYR2eZvKYlo2CQ2ieV9S5',
-    items: [{ price: 'price_1JZUhRSIUZLasS08U1banUmU' }],
+    items: [{ price: "price_1JZUhRSIUZLasS08U1banUmU" }],
   });
   res.status(201).json({
-    status: 'Success',
+    status: "Success",
     data: subscription,
   });
 });
 
+// Create session
+// const createSession = asyncHanlder(async (req, res, next) => {
+//   const stripe = Stripe(stripeSecretKey);
+
+//   const session = await stripe.checkout.sessions.create({
+//     success_url: "http://localhost:4500/paymentSuccess",
+//     cancel_url: "http://localhost:4500/",
+//     payment_method_types: ["card"],
+//     line_items: [{ price: "price_1JZUhSSIUZLasS08dEUajjQl", quantity: 1 }],
+//     mode: "subscription",
+//     customer: "cus_KE5AtPD46hCuWa",
+//   });
+
+//   console.log(session);
+
+//   res.status(200).json({
+//     status: "success",
+//   });
+// });
+const createSession = asyncHanlder(async (req, res, next) => {
+  const stripe = Stripe(stripeSecretKey);
+
+  const session = await stripe.checkout.sessions.create({
+    success_url: "http://localhost:4500/paymentSuccess",
+    cancel_url: "http://localhost:4500/",
+    payment_method_types: ["card"],
+    line_items: [{ price: "price_1JZdjbSIUZLasS087dwqesuG", quantity: 1 }],
+    mode: "subscription",
+    customer: "cus_KE5AtPD46hCuWa",
+  });
+
+  console.log(session);
+
+  res.status(200).json({
+    status: "success",
+  });
+});
+
 // Pass Name as query to create customer
-app.get('/createCustomer', createCharges);
-app.get('/getCustomer', getCustomerFromStripe);
+app.get("/createCustomer", createCharges);
+app.get("/getCustomer", getCustomerFromStripe);
 
 // Create Subscription
-app.get('/createSubscription', createSubscription);
+app.get("/createSubscription", createSubscription);
+app.get("/createSession", createSession);
+
+app.get("/paymentSuccess", (req, res) => {
+  res.status(200).json({
+    status: "Payment Success",
+  });
+});
+
+app.get("/", (req, res) => {
+  res.status(200).json({
+    status: "Success",
+    message: "Welcome to stripe Test",
+  });
+});
 
 module.exports = app;
